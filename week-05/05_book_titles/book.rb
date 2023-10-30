d03d14230f3c5b3b515d91245fd76f0b82618878
akabishau <aleksey.kabishau@icloud.com>
2023-10-29 23:36:48 -0400

    complete assignment 05

--- week-05/05_book_titles/book.rb
+++ week-05/05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    def initialize
+        @title = ""
+    end
+
+    # setter logic
+    def title=(prompt)
+        ignore_list = ["in", "of", "a", "an", "and", "the"]
+        words = prompt.split.map.with_index do | word, index |
+            ignore_list.include?(word) && index != 0 ? word : word.capitalize
+        end
+        @title = words.join(" ")
+    end
 end
