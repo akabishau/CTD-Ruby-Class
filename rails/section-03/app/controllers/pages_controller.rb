b97da775a2dcf1334583e0a09705feeab9caaeef
akabishau <aleksey.kabishau@icloud.com>
2023-11-04 13:24:05 -0400

    complete create actions part of the tutorial

--- rails/section-03/app/controllers/pages_controller.rb
+++ rails/section-03/app/controllers/pages_controller.rb
@@ -9,4 +9,22 @@ class PagesController < ApplicationController
         @page = Page.find(params[:id])
         render :show
     end
+
+    # get new form
+    def new
+        @page = Page.new
+        render :new
+    end
+
+    # post submit form with values available in params object
+    def create
+        @page = Page.new(post_params)
+        @page.save
+        redirect_to @page # browser will request page template view
+    end
+
+    private
+    def post_params
+        params.require(:page).permit(:title, :body, :slug)
+    end
 end
--- rails/section-03/config/routes.rb
+++ rails/section-03/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
   get "/pages", to: "pages#index"
+  get "/pages/new", to: "pages#new", as: "new_page"
   get "/pages/:id", to: "pages#show", as: "page"
+
+  # method, resource, controller's method
+  post "pages", to: "pages#create"
 end
