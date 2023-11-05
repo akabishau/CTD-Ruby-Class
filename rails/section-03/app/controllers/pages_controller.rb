c898729f8decd818e2b1b7051f781d7fe5c4ef57
akabishau <aleksey.kabishau@icloud.com>
2023-11-04 22:14:36 -0400

    complete update actions part of the tutorial

--- rails/section-03/app/controllers/pages_controller.rb
+++ rails/section-03/app/controllers/pages_controller.rb
@@ -10,6 +10,11 @@ class PagesController < ApplicationController
         render :show
     end
 
+    def edit
+        @page = Page.find(params[:id])
+        render :edit
+    end
+
     # get new form
     def new
         @page = Page.new
@@ -18,13 +23,22 @@ class PagesController < ApplicationController
 
     # post submit form with values available in params object
     def create
-        @page = Page.new(post_params)
+        @page = Page.new(page_params)
         @page.save
         redirect_to @page # browser will request page template view
     end
 
-    private
-    def post_params
-        params.require(:page).permit(:title, :body, :slug)
+    
+
+    def update
+        @page = Page.find(params[:id])
+        @page.update(page_params)
+        redirect_to @page
     end
+
+
+    private
+        def page_params
+            params.require(:page).permit(:title, :body, :slug)
+        end
 end
--- rails/section-03/config/routes.rb
+++ rails/section-03/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "/pages", to: "pages#index"
   get "/pages/new", to: "pages#new", as: "new_page"
   get "/pages/:id", to: "pages#show", as: "page"
-
+  get "/pages/:id/edit", to: "pages#edit", as: "edit_page"
   # method, resource, controller's method
   post "pages", to: "pages#create"
+  patch "/pages/:id", to: "pages#update"
 end
