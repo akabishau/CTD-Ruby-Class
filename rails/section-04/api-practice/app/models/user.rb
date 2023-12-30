e16a01bd616225719702e451df8bd738140aaee1
akabishau <aleksey.kabishau@icloud.com>
2023-12-29 22:36:32 -0500

    complete basic authentication

--- rails/section-04/api-practice/app/models/user.rb
+++ rails/section-04/api-practice/app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, email: true
   validates :password, password_strength: true
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 end
